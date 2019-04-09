ebcf49c3decd175ccd3a6c3eb934aecd3afaa8cf
Clement Chandon <clement.chandon@ipsa.fr>
2019-04-09 16:38:22 +0200

    Update slideshows.js

--- src/slideshows/slideshows.js
+++ src/slideshows/slideshows.js
@@ -2,10 +2,10 @@
 // website, don't forget to register your new slideshows in the list:
 export default {
   list: [
+    require('./Ma321/Presentation').default,
     require('./introducing-eagle/IntroducingEagle').default,
     require('./eagle-recipes/EagleRecipes').default,
     require('./themes-slideshow/ThemesSlideshow').default,
-    require('./first-slideshow/FirstSlideshow').default,
-    require('./Ma321/Presentation').default
+    require('./first-slideshow/FirstSlideshow').default
   ]
 }
